96b926378d885fd0e8fc4b9c006c7b9dae070ef1
krakenfull25 <krakenfull25@gmail.com>
2025-06-01 20:40:39 +0200

    comienzo del proyectov2

--- src/main/java/daw/ProyectoFinalv1.java
+++ src/main/java/daw/ProyectoFinalv1.java
@@ -12,5 +12,8 @@ public class ProyectoFinalv1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(""
+                + "");
+                
     }
 }
