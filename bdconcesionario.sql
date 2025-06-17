126e773960972ee1290e0eb00d418d310480fcf3
jgarvaz <jgarvaz2505@g.educaand.es>
2025-06-17 08:44:49 +0200

    Update bdconcesionario.sql

--- bdconcesionario.sql
+++ bdconcesionario.sql
@@ -1,10 +1,10 @@
 -- Crear base de datos
-DROP DATABASE IF EXISTS BDconcesionario;
-CREATE DATABASE BDconcesionario;
-USE BDconcesionario;
+DROP DATABASE IF EXISTS bdconcesionario;
+CREATE DATABASE bdconcesionario;
+USE bdconcesionario;
 
 -- Tabla Marcas
-CREATE TABLE Marcas (
+CREATE TABLE marcas (
 	idMarca INT AUTO_INCREMENT PRIMARY KEY,
 	nombre VARCHAR(100),
 	paisOrigen VARCHAR(100),
@@ -12,53 +12,52 @@ CREATE TABLE Marcas (
 );
 
 -- Tabla Coches
-CREATE TABLE Coches (
+CREATE TABLE coches (
 	idCoche INT AUTO_INCREMENT PRIMARY KEY,
 	modelo VARCHAR(100),
 	anio YEAR,
 	precio DECIMAL(10,2),
 	tipoMotor VARCHAR(50),
 	idMarca INT,
-	FOREIGN KEY (idMarca) REFERENCES Marcas(idMarca)
+	FOREIGN KEY (idMarca) REFERENCES marcas(idMarca)
 );
 
 -- Tabla Accesorios
-CREATE TABLE Accesorios (
+CREATE TABLE accesorios (
 	idAccesorio INT AUTO_INCREMENT PRIMARY KEY,
 	nombre VARCHAR(100),
 	descripcion TEXT
 );
 
 -- Tabla intermedia CocheAccesorio (N:M)
-CREATE TABLE CocheAccesorio (
+CREATE TABLE cocheaccesorio (
 	idRegistro INT AUTO_INCREMENT PRIMARY KEY,
 	idCoche INT,
 	idAccesorio INT,
-	
-	FOREIGN KEY (idCoche) REFERENCES Coches(idCoche),
-	FOREIGN KEY (idAccesorio) REFERENCES Accesorios(idAccesorio)
+	FOREIGN KEY (idCoche) REFERENCES coches(idCoche),
+	FOREIGN KEY (idAccesorio) REFERENCES accesorios(idAccesorio) ON DELETE SET NULL
 );
 
 -- Insertar datos en Marcas
-INSERT INTO Marcas (nombre, paisOrigen, fundacion) VALUES
+INSERT INTO marcas (nombre, paisOrigen, fundacion) VALUES
 ('Toyota', 'Japon', 1937),
 ('Ford', 'EE.UU.', 1903),
 ('BMW', 'Alemania', 1916);
 
 -- Insertar datos en Coches
-INSERT INTO Coches (modelo, anio, precio, tipoMotor, idMarca) VALUES
+INSERT INTO coches (modelo, anio, precio, tipoMotor, idMarca) VALUES
 ('Corolla', 2022, 22000.00, 'Gasolina', 1),
 ('Mustang', 2023, 45000.00, 'Gasolina', 2),
 ('X5', 2021, 60000.00, 'Híbrido', 3);
 
 -- Insertar datos en Accesorios
-INSERT INTO Accesorios (nombre, descripcion) VALUES
+INSERT INTO accesorios (nombre, descripcion) VALUES
 ('GPS', 'Sistema de navegación integrado'),
 ('Cámara Trasera', 'Cámara para asistencia de reversa'),
 ('Techo Solar', 'Techo corredizo de cristal');
 
 -- Insertar datos en CocheAccesorio
-INSERT INTO CocheAccesorio(idCoche,idAccesorio) VALUES
+INSERT INTO cocheaccesorio (idCoche,idAccesorio) VALUES
 (1, 1),  -- Corolla tiene GPS
 (1, 2),  -- Corolla tiene Cámara Trasera
 (2, 2),  -- Mustang tiene Cámara Trasera
